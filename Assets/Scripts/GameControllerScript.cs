2b3550a5797e9e591a3b18364addd8bc2f995f49
Данил Чичиланов <danil.chichilanov@gmail.com>
2023-04-05 22:13:22 +0700

    Что-то нахимичил с башнями

--- Assets/Scripts/GameControllerScript.cs
+++ Assets/Scripts/GameControllerScript.cs
@@ -5,19 +5,29 @@ using UnityEngine;
 public class Tower
 {
     public float range, CoolDown, CurrentCoolDown = 0;
+    public Sprite Spr;
 
-    Tower(float range, float coolDown)
+    public Tower(float range, float coolDown, string path)
     {
         this.range = range;
         CoolDown = coolDown;
-
+        Spr = Resources.Load<Sprite>(path);
     }
 
 }
 
 public class TowerProjectile
-{ 
-    
+{
+    public float speed;
+    public int damage;
+    public Sprite Spr;
+
+    public TowerProjectile(float speed, int dmg, string path)
+    {
+        this.speed = speed;
+        damage = dmg;
+        Spr = Resources.Load<Sprite>(path);
+    }
 }
 
 public enum TowerType
@@ -29,5 +39,15 @@ public enum TowerType
 
 public class GameControllerScript : MonoBehaviour
 {
-    
+    public List<Tower> AllTowers = new List<Tower>();
+    public List<TowerProjectile> AllProjectiles = AllProjectiles = new List<TowerProjectile>();
+
+    private void Awake()
+    {
+        AllTowers.Add(new Tower(2,.3f, "TowerSprites/FastTower"));
+        AllTowers.Add(new Tower(5, 1, "TowerSprites/SlowTower"));
+
+        AllProjectiles.Add(new TowerProjectile(7, 10, "ProjectilesSprites/FastTower"));
+        AllProjectiles.Add(new TowerProjectile(3, 15, "ProjectilesSprites/SlowTower"));
+    }
 }
--- Assets/Scripts/TowerScript.cs
+++ Assets/Scripts/TowerScript.cs
@@ -4,23 +4,35 @@ using UnityEngine;
 
 public class TowerScript : MonoBehaviour
 {
-    [SerializeField] private float _range;
-    [SerializeField] private float _CurrentCoolDown, _CoolDown;
+    
     [SerializeField] private GameObject _projectile;
+    Tower selfTower;
+    public TowerType selfType;
+    GameControllerScript gcs;
 
+
+    private void Start()
+    {
+        gcs = FindObjectOfType<GameControllerScript>();
+
+        selfTower = gcs.AllTowers[(int)selfType];
+
+        GetComponent<SpriteRenderer>().sprite = selfTower.Spr;
+
+    }
 
     private void Update()
     {
         if (CanShoot())
             FindTarget();
 
-        if (_CurrentCoolDown > 0)
-            _CurrentCoolDown -= Time.deltaTime;
+        if (selfTower.CurrentCoolDown > 0)
+            selfTower.CurrentCoolDown -= Time.deltaTime;
     }
 
     private bool CanShoot()
     {
-        if (_CurrentCoolDown <= 0)
+        if (selfTower.CurrentCoolDown <= 0)
             return true;
         return false;
     }
@@ -34,7 +46,7 @@ public class TowerScript : MonoBehaviour
         {
             float currentDistance = Vector2.Distance(transform.position, enemy.transform.position);
 
-            if (currentDistance < nearestEnemyDistance && currentDistance <= _range)
+            if (currentDistance < nearestEnemyDistance && currentDistance <= selfTower.range)
             {
                 nearestEnemy = enemy.transform;
                 nearestEnemyDistance = currentDistance;
@@ -52,7 +64,7 @@ public class TowerScript : MonoBehaviour
 
     private void Shoot(Transform enemy)
     {
-        _CurrentCoolDown = _CoolDown;
+        selfTower.CurrentCoolDown = selfTower.CoolDown;
 
         GameObject projectile = Instantiate(_projectile);
         projectile.transform.position = transform.position;
