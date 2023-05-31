e7ba52c83c94292b55835396bfa9f35b8fee0fdd
HornyGlory <mushrooms2016@mail.ru>
2023-05-31 16:10:45 +0700

    as

--- Assets/Scripts/Other/Music/Music.cs
+++ Assets/Scripts/Other/Music/Music.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Music : MonoBehaviour
 {
+    public Sprite onMusic;
+    public Sprite offMusic;
+
+    public Image MusicButton;
+    public bool isOn;
+    public AudioSorce ad;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        isOn= true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (PlayerPref)
+        {
+
+        }
     }
 }
